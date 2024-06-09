da5c73f68d43391302010fd0fd445bb825ba5366
Taher Shabbir <taherihh53@gmail.com>
2024-06-09 03:23:36 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+//assignment no 6
+var nameWithWhiteSpace = "\t \n ambreen taher \t \n";
+console.log("name With White Space", nameWithWhiteSpace);
+var strippedName = nameWithWhiteSpace.trim();
+console.log("stripped Name:", strippedName);
---
+++ main.ts
@@ -0,0 +1,6 @@
+//assignment no 6
+let nameWithWhiteSpace:string = "\t \n ambreen taher \t \n";
+console.log("name With White Space", nameWithWhiteSpace);
+
+let strippedName:string = nameWithWhiteSpace.trim();
+console.log("stripped Name:", strippedName);
